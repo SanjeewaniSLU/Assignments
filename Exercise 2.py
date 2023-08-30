bbc80a4abaca5180c0d6cbc6ae0b45f0fe375f06
upeksha <slusanjeewani@gmail.com>
2023-08-30 23:51:53 +0300

    1st commit 2nd commit

--- Exercise 2.py
+++ Exercise 2.py
@@ -1,18 +1,22 @@
 import math
+#1.
 #name = input("What is your name?")
 #print(f"Hello,{name}")
 
+#2.
 #radius = float(input("Enter radius of the circle:"))
 #area = math.pi * radius **2
 #print(f"The are of the circle:{area:.2f}")
 
+#3.
 #length = float(input("Enter the length of rectangle:"))
 #width = float(input("Enter the width of rectangle:"))
-#perimeter = (length*2)+(width*2)
+#perimeter = (length + width) * 2
 #area = length*width
 #print(f"The perimeter of the rectangle:{perimeter:.2f}")
 #print(f"The area of the rectangle:{area:.2f}")
 
+#4.
 #number1 =float(input("Enter one integer numbers:"))
 #number2 =float(input("Enter another integer numbers:"))
 #number3 =float(input("Enter one more integer numbers:"))
@@ -23,6 +27,7 @@ import math
 #print(f"product of three integer numbers:{product}")
 #print(f"average of three integer numbers:{average}")
 
+#5.
 talents = float(input("Enter talents:"))
 pounds = float(input("Enter pounds:"))
 lots = float(input("Enter lots:"))
@@ -36,6 +41,5 @@ mass_kg = mass // 1000
 mass_g = mass % 1000
 mass_kg_int = int(mass_kg)
 print(f"The weight in modern units: \n {mass_kg_int} kilograms and {mass_g:.2f} grams.")
-
 
 
