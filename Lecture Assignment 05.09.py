63ea8cccef1e4fa5ffee6d87d31ecf0fca51db59
upeksha <slusanjeewani@gmail.com>
2023-09-05 15:20:56 +0300

    1st commit 2nd commit 3rd commit 4th commit

--- Lecture Assignment 05.09.py
+++ Lecture Assignment 05.09.py
@@ -1,3 +1,4 @@
+"""
 list=[]
 while True:
     number = int(input("Enter a number:"))
@@ -7,3 +8,15 @@ while True:
     list.append(number)
 print("Numbers you have entered:")
 print(list)
+
+"""
+shop=[]
+while True:
+    cart = input("What you bought from shop:")
+    if cart == 'done':
+        break
+    else:
+        shop.append(cart)
+print("\n shopping list")
+for index,item in enumerate(shop,start=1):
+    print(f"{index}.{item}")
