7689d1de7b730e1413f18990d9bfae772e87ae21
upeksha <slusanjeewani@gmail.com>
2023-10-16 11:22:48 +0300

    1st commit 2nd commit 3rd commit 4th commit 5tt commit 6th commit

--- Exercise 3.py
+++ Exercise 3.py
@@ -1,11 +1,11 @@
 
 #1
 
-z =float (input("Length of a zander in cm"))
+z =float (input("Length of a zander in cms: "))
 if z>=42:
     print("Zander fulfills the required length. " )
 else:
-    print(f"Release the zander into tank.It's {42-z}cm below the required length.")
+    print(f"Release the zander into tank.It's {42-z}cms below the required length.")
 
 
 #2
