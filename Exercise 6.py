7ecb7b137e2e117ae6d53dedbab96a2afaac941c
upeksha <slusanjeewani@gmail.com>
2023-10-17 21:06:26 +0300

    1st commit 2nd commit 3rd commit 4th commit 5tt commit 6th commit

--- Exercise 6.py
+++ Exercise 6.py
@@ -1,7 +1,7 @@
 #1
 
 import random
-
+"""
 def dice_roll():
     return random.randint(1,6)
 
@@ -12,3 +12,19 @@ while True:
     if result == 6:
         break
 
+#2
+
+
+def dice_roll(num_sides_dice):
+    return random.randint(1,num_sides_dice)
+
+
+num_sides_dice = int(input("Enter the number of sides on the dice: "))
+
+while True:
+    result = dice_roll(num_sides_dice)
+    print(f"You rolled the dice, {result}")
+    if result == num_sides_dice:
+        break
+"""
+#3
