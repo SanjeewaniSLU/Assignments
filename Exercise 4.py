14fbbac5e62df9a2840d3c9ecb55a4238dff4a33
upeksha <slusanjeewani@gmail.com>
2023-10-17 16:54:52 +0300

    1st commit 2nd commit 3rd commit 4th commit 5tt commit 6th commit

--- Exercise 4.py
+++ Exercise 4.py
@@ -85,9 +85,9 @@ m = 1
 #Total number of points in cirlce
 n = 0
 
-while m <=N:
-     x=random.uniform(-1,1)
-     y=random.uniform(-1,1)
+while m <= N:
+     x = random.uniform(-1,1)
+     y = random.uniform(-1,1)
      if x ** 2 + y ** 2 <= 1:
          n += 1
      m = m + 1
