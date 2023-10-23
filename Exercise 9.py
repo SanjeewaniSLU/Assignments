7323f677d4e6ecec49f9800e3b61c87f87292eb1
upeksha <slusanjeewani@gmail.com>
2023-10-23 23:15:47 +0300

    1st commit 2nd commit 3rd commit 4th commit 5tt commit 6th commit 7th commit

--- Exercise 9.py
+++ Exercise 9.py
@@ -2,46 +2,14 @@
 #1
 class car():
     def __init__(self,r,m,c,t):
-        self.Registration_number = r
-        self.Maximum_speed = m
-        self.Current_speed = c
-        self.Travelled_distance = t
+        self.registration_number = r
+        self.maximum_speed = m
+        self.current_speed = c
+        self.travelled_distance = t
 
+new_car =car("ABC-123","142 km/h","0","0")
+print(f"Properties of the new car:-\nRegistarion number-",new_car.registration_number,"\nMaximum speed-",
+      new_car.maximum_speed,"\nCurrent Speed-",new_car.current_speed,"km/h\nTravelled Distance-",
+      new_car.travelled_distance,"km")
 
-new_car =car("ABC-123","142 km","0","0")
-print(f"Properties of the new car:-\nRegistarion number",new_car.Registration_number,"\nMaximum speed-",new_car.Maximum_speed,"\nCurrent Speed-",new_car.Current_speed,"\nTravelled Distance-",new_car.Travelled_distance)
-# class pet():
-#     counter = 0
-#
-#     def __init__(self,n,c):
-#         self.fName = n
-#         self.color = c
-#         pet.counter += 1
-#
-#     def bark(self,n):
-#         for i in  range(n):
-#             print("Bark,Bark")
-#
-#     def setName(self,n):
-#         self.fName = n
-#         print ("Name is changed")
-#
-#     def getColor(self):
-#         return self.color
-#
-# dog = pet("Snowy","White")
-# cat = pet("Chick","Grey")
-# snake = pet("Python","Yellow")
-#
-# print(dog.fName,dog.color)
-# print(cat.fName,cat.color)
-# print(snake.fName,snake.color)
-#
-# dog.bark(5)
-# print("My snake also bark,see")
-# snake.bark(2)
-#
-# dog.setName("Bow")
-# print(dog.fName)
-# print(f"Here is the dog color",dog.getColor())
-# print("Number of objects", pet.counter)
+#2
