715430fd034f175321ebe9e2b7e6fe77d16e71ef
upeksha <slusanjeewani@gmail.com>
2023-10-29 00:04:44 +0300

    1st commit 2nd commit 3rd commit 4th commit 5tt commit 6th commit 7th commit 8th commit

--- Exercise 9.py
+++ Exercise 9.py
@@ -1,9 +1,11 @@
+# Exercise -1
 class Car:
-    def __init__(self, registration_number, max_speed):
-        self.registration_number = registration_number
+    def __init__(self,reg_no, max_speed):
+        self.reg_no = reg_no
         self.max_speed = max_speed
         self.current_speed = 0
         self.travelled_distance = 0
+# Exercise -2
 
     def accelerate(self, change):
         new_speed = self.current_speed + change
@@ -13,16 +15,17 @@ class Car:
             self.current_speed = self.max_speed
         else:
             self.current_speed = new_speed
+# Exercise -3
 
     def drive(self, hours):
-        self.travelled_distance = self.current_speed * hours
+        distance = self.current_speed * hours
+        self.travelled_distance += distance
 
 
 my_car = Car("ABC-123", 142)
 
-print(f"Properties of the new car:\n\nRegistration Number:", my_car.registration_number,
-      "\nMaximum Speed:", my_car.max_speed, " km/h."
-                                            " \nCurrent Speed:", my_car.current_speed, " km/h\nTravelled Distance:",
+print(f"Properties of the new car:\n\nRegistration Number:", my_car.reg_no,
+      "\nMaximum Speed:", my_car.max_speed, " km/h.""\nCurrent Speed:", my_car.current_speed, " km/h\nTravelled Distance:",
       my_car.travelled_distance, "km")
 
 my_car.accelerate(30)
@@ -39,51 +42,35 @@ my_car.accelerate(60)
 my_car.drive(1.5)
 print(f"New Travelled Distance:", my_car.travelled_distance, "km")
 
+# Exercise -4
+
+
 import random
-
-
-class Car:
-    def __init__(self, registration_number, max_speed):
-        self.registration_number = registration_number
-        self.max_speed = max_speed
-        self.current_speed = 0
-        self.travelled_distance = 0
-
 
 cars = []
 
-
-def random_speed():
-    return random.randint(100, 200)
-
-
 for i in range(1, 11):
     registration_number = f"ABC-{i}"
-    maximum_speed = random_speed()
+    maximum_speed = random.randint(100, 200)
     new_car = Car(registration_number, maximum_speed)
     cars.append(new_car)
 
+
+def accelerate(self,current_speed):
+    change_speed = random.randint(-15, 10)
+    self.current_speed += change_speed
+
+
+def drive(self, hours=1):
+        self.travelled_distance = self.current_speed * 1
+
+
 for car in cars:
-    print(f"Registration Number:", car.registration_number)
+    print(f"Registration Number:", car.reg_no)
     print(f"Maximum Speed:", car.max_speed, " km/h")
-    print(f"Current Speed:", car.current_speed, " km/h")
-    print(f"Travelled Distance:", car.travelled_distance, " km")
+    print(f"Current Speed:", self.current_speed, " km/h")
+    # print(f"Travelled Distance:", car.travelled_distance, " km")
     print("-" * 30)
 
 
-    def accelerate(self, change):
-        change = random.randint(-15, 10)
-        new_speed = self.current_speed + change
-        if new_speed < 0:
-            self.current_speed = 0
-        elif new_speed > self.max_speed:
-            self.current_speed = self.max_speed
-        else:
-            self.current_speed = new_speed
 
-
-    def drive(self, hours):
-        self.travelled_distance = self.current_speed * hours
-
-car.travelled_distance(10000)
-car.drive(1)
