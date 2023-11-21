0fea8ca7e5e3c8f7930fffd5a4c2e38dcd801470
upeksha <slusanjeewani@gmail.com>
2023-11-21 17:42:38 +0200

    3 rd commit 4th commit

---
+++ Exercise 13.py
@@ -0,0 +1,25 @@
+# -1
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+def prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+@app.route('/prime_number/<int:number>', methods=['GET'])
+def check_prime_num(number):
+    result = {"Number": number, "is Prime": prime(number)}
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
+
+# -2
