9194dbfc083dd3baeb8cedf2d390e180bf1fc4f5
upeksha <slusanjeewani@gmail.com>
2023-11-21 19:55:10 +0200

    3 rd commit 4th commit 5th commit

--- Exercise 13.py
+++ Exercise 13.py
@@ -35,3 +35,39 @@ if __name__ == '__main__':
 
 # -2
 
+from flask import Flask, Response
+import json
+
+app = Flask(__name__)
+
+airport_data = {
+    "EFHK": {"Name": "Helsinki-Vantaa Airport", "Location": "Helsinki"},
+    "EGFF": {"Name": "Cardiff International Airport", "Location": "Cardiff"},
+    "EGGD": {"Name": "Bristol Airport", "Location": "Bristol"},
+    "EGSC": {"Name": "Cambridge Airport", "Location": "Cambridge"},
+    "VCBI": {"Name": "Bandaranaike International Colombo Airport", "Location": "Colombo"}
+}
+
+
+@app.route('/airport/<icao_code>')
+def get_airport_info(icao_code):
+    icao_code = icao_code.upper()
+    if icao_code in airport_data:
+        airport_info = airport_data[icao_code]
+        response_data = {
+            "ICAO": icao_code,
+            "Name": airport_info["Name"],
+            "Location": airport_info["Location"]
+        }
+        json_response = json.dumps(response_data)
+        http_response = Response(response=json_response, status=200, mimetype="application/json")
+        return http_response
+    else:
+        error_response = {"error": "Airport not found"}
+        json_error_response = json.dumps(error_response)
+        http_error_response = Response(response=json_error_response, status=404, mimetype="application/json")
+        return http_error_response
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
